f380f4f93a0a875fb5365f57a970494a45d943d8
Personal CAD CAPMGMT N <CAPMGMT@IUSER>
2020-03-03 17:17:44 +0000

    03/03/2020 S9. 1. the koa app in typescript is now supporting async/await in transpiling, due to adding @babel/plugin-transform-runtime and @babel/runtime

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 export default function createServer() {
   const server = new Koa();
-
+  // now define the error middleware, to catch all the untreated errors in the subsequent middleware
+  server.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err){
+      ctx.status = err.status || 500;
+      ctx.body = err.message;
+    }
+  });
   server.use(router.allowedMethods());
   server.use(router.routes());
   return server;
