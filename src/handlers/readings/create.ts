355da91b9ff241482074be286cb557ecd0e7124c
Personal CAD CAPMGMT N <CAPMGMT@IUSER>
2020-03-03 11:39:34 +0000

    03/03/2020 S2. 1. Now the API respond with a code 400 BAD REQUEST to a POST of an empty payload

--- src/handlers/readings/create.ts
+++ src/handlers/readings/create.ts
@@ -1,18 +1,15 @@
 import {ExtendableContext, Response} from 'koa';
 
 export const createReading = (ctx: ExtendableContext, next: () => Promise<any>) => {
-  console.log('BBBBBBBBBBBBBBBBBBBBBBBBBBBBB ctx.body', ctx.body)
   if (!ctx?.body){
+      ctx.response.status = 400; // Error, BAD REQUEST
       ctx.body = {
-        status: 400,
         data: {message: 'Payload should not be empty'},
       };
       return
   }
-  // the server responds with 200
-  //ctx.set('Content-Type','text/plain');
+  // the server responds with 200 by default
   return ctx.body = {
-    status: 200,
     data: {message: 'OK'},
   };
 };
--- src/spec/cucumber/steps/request.ts
+++ src/spec/cucumber/steps/request.ts
@@ -2,6 +2,7 @@ import { When, Then } from 'cucumber';
 import superagent from 'superagent';
 import {Request, Response} from 'koa';
 import {ServerResponse} from 'http';
+import {AssertionError} from 'assert';
 
 interface Error{
   response?: unknown;
@@ -10,10 +11,7 @@ interface Error{
 
 interface Payload {
   status: number;
-  data: {
-    message: string;
-    payload: {[key:string]: string|number}
-  }
+  text: string
 }
 
 let request: superagent.SuperAgentRequest;
@@ -35,12 +33,15 @@ When('sends the request', function(cb){
   // @ts-ignore
   request
     .then((response) => {
-      result = response.body;
-      header = response.header;
+      console.log('TTTTTTTTTTTTTTTTTTTTTTTTTTTTT response=', response)
+      result = response?.body;
+      header = response?.header;
       cb();
     })
     .catch((errResponse: { response: Error; }) =>{
+      // console.log('EEEEEEEEEEEEEEEEEEEEEEEEEEEEEE errorResponse=', errResponse);
       error = errResponse.response;
+      // console.log('EEEEEEEEEEEEEEEEEEEEEEEEEEEEEE error=', error);
       cb();
     })
 });
@@ -48,7 +49,10 @@ When('sends the request', function(cb){
 Then('our API should respond with a 400 HTTP status code', function () {
   {
     if (error && error.statusCode && error.statusCode !==400) {
-      throw new Error();
+      throw new AssertionError({
+        expected: 400,
+        actual: response?.status
+      });
     }
   };
 });
@@ -58,11 +62,13 @@ Then('the header of the response should include {string}', function(string) {
   switch (string) {
     case 'application/json':
       let contentType!: string;
-      // @ts-ignore
-      contentType = header['content-type'];
-      if (contentType && contentType.length === 0
-        || (typeof contentType === 'string' && !contentType.includes(string))) {
-        throw new Error('Response not of Content-Type application/json');
+      if (header) {
+        // @ts-ignore
+        contentType = header['content-type'];
+        if (contentType && contentType.length === 0
+          || (typeof contentType === 'string' && !contentType.includes(string))) {
+          throw new Error('Response not of Content-Type application/json');
+        }
       }
   }
 });
@@ -78,7 +84,8 @@ Then('the payload of the response should be a valid JSON object', function(){
 
 
 Then('contains a message property which says "Payload should not be empty"', function(){
-  if(payload?.data?.message !== 'Payload should not be empty'){
+  // console.log('PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP payload=', payload)
+  if(!payload?.text.includes('Payload should not be empty')){
     throw new Error('Invalid response for empty payload');
   }
 });
